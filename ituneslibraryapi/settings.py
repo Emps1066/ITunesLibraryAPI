79a37e0e4c344371f6ab5a3ca5b09617a3fa6b0b
HarryLuke1415 <harryluke1415@gmail.com>
2021-01-06 22:07:59 +1100

    Removed secret key

--- ituneslibraryapi/settings.py
+++ ituneslibraryapi/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#^r2o)egu)b9rriabshi(n%74tc2ro1264%44ykj_(mfgtbw+q'
+SECRET_KEY =
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
