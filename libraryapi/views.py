c46fb949f903438310d95d4da463aa96d4cfe035
HarryLuke1415 <harryluke1415@gmail.com>
2021-01-11 00:27:13 +1100

    finished up basic url stuff

--- libraryapi/views.py
+++ libraryapi/views.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets
 from .models import Song
 from .serializers import SongSerializer
 
+
 class SongViewSet(viewsets.ModelViewSet):
     queryset = Song.objects.all().order_by('name')
     serializer_class = SongSerializer
-
